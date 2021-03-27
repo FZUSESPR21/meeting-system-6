0dc78201ac3b34be3b7b5360f9cac99521438ea6
songxiren <1632769437@qq.com>
2021-03-27 16:01:29 +0800

    Create Forums.java

---
+++ backend/src/domain/Forums.java
@@ -0,0 +1,33 @@
+public class Forums {
+    private String forumnama;
+    private String theme;
+    private String chairman;
+    private String time;
+    public String getForumnama(){
+        return forumnama;
+    }
+    public String getTheme(){
+        return theme;
+    }
+    public String getChairman(){
+        return chairman;
+    }
+    public String getTime(){
+        return time;
+    }
+    public void setForumnama(String forumnama) {
+        this.forumnama = forumnama;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+    public void setChairman(String chairman) {
+        this.chairman = chairman;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
